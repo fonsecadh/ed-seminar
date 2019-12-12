17f05db38d915f6e8c0129c37abfb5c626634634
fonsecadh <UO258318@uniovi.es>
2019-12-12 22:22:29 +0100

    Lua Latex was not working for some reason.

--- trees/compileReport.sh
+++ trees/compileReport.sh
@@ -1,1 +1,2 @@
-pandoc ed-trees-seminar-uo258318.md -p --pdf-engine=lualatex --toc --variable 'geometry:margin=1in' -o ed-trees-seminar-uo258318.pdf
+pandoc ed-trees-seminar-uo258318.md --toc -o ed-trees-seminar-uo258318.pdf
+
